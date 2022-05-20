c02ba915a12c03cdc7e08fefb02128e7cdc73f67
Sparta142 <Sparta142@users.noreply.github.com>
2022-05-20 00:30:40 -0400

    Fix comments in ip.go

--- ffxiv/ip.go
+++ ffxiv/ip.go
@@ -2,7 +2,8 @@ package ffxiv
 
 import "net"
 
-// All known public FINAL FANTASY XIV data center IP networks, in CIDR notation.
+// DataCenterCIDRs is an array of all known public FINAL FANTASY XIV
+// data center IP networks, in string CIDR notation.
 var DataCenterCIDRs = [...]string{
 	"204.2.229.0/24",   // North America: Aether, Crystal, Primal
 	"195.82.50.0/24",   // Europe: Chaos, Light
@@ -10,8 +11,9 @@ var DataCenterCIDRs = [...]string{
 	"153.254.80.0/24",  // Oceania: Materia
 }
 
-// All known public FINAL FANTASY XIV data center IP networks.
-var DataCenterNets []net.IPNet
+// DataCenterNets is a list of all known public
+// FINAL FANTASY XIV data center IP networks, as IPNets.
+var DataCenterNets = make([]net.IPNet, len(DataCenterCIDRs))
 
 // Returns whether ip is a known FINAL FANTASY XIV address.
 func IsFinalFantasyIP(ip net.IP) bool {
@@ -25,8 +27,6 @@ func IsFinalFantasyIP(ip net.IP) bool {
 }
 
 func init() {
-	DataCenterNets = make([]net.IPNet, len(DataCenterCIDRs))
-
 	for i, s := range DataCenterCIDRs {
 		_, net, err := net.ParseCIDR(s)
 		if err != nil {
