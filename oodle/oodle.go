4fef15f405f9a3ab87cbbec54dc981d71bea5db4
Sparta142 <Sparta142@users.noreply.github.com>
2022-09-27 20:13:36 -0400

    Actually fix cgo issue when not on windows-amd64

--- oodle/oodle.go
+++ oodle/oodle.go
@@ -1,8 +1,11 @@
-//go:build !(windows && amd64)
+//go:build !(cgo && windows && amd64)
 
 package oodle
 
-import "errors"
+import (
+	"C" // To prevent "C source files not allowed when not using cgo or SWIG"
+	"errors"
+)
 
 var ErrPlatformNotSupported = errors.New("oodle: platform not supported")
 
